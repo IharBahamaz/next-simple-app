9ec505ba501352e4131c73f16e806c240b9c1ac6
IllusoryedoC <codeoffantasy@gmail.com>
2023-09-27 00:52:53 +0200

    Deleting an Object

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -4,9 +4,6 @@ export function GET(
   request: NextRequest,
   { params }: { params: { id: number } }
 ) {
-  //Fetch data from a db
-  //If not found, return 404 error
-  //Else return data
   if (params.id > 10)
     return NextResponse.json({ error: 'User not found' }, { status: 404 });
 
@@ -17,16 +14,22 @@ export async function PUT(
   request: NextRequest,
   { params }: { params: { id: number } }
 ) {
-  // Validate the request body
   const body = await request.json();
   if (!body.name)
     return NextResponse.json({ error: 'Name is required' }, { status: 400 });
-  // If invalid , return 400
-  // Fetch the user with the given id
-  // If doesn't exist, return 404
+
   if (params.id > 10)
     return NextResponse.json({ error: 'User not found' }, { status: 404 });
-  // Return the updated user
-  // Update the user
+
   return NextResponse.json({ id: 1, name: body.name });
+}
+
+export function DELETE(
+  request: NextRequest,
+  { params }: { params: { id: number } }
+) {
+  if (params.id > 10)
+    return NextResponse.json({ error: 'User not found' }, { status: 404 });
+
+  return NextResponse.json({});
 }
