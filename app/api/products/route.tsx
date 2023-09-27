da467753389d94e8b0ffb04ae57f3521b249bd3a
IllusoryedoC <codeoffantasy@gmail.com>
2023-09-28 00:23:16 +0200

    products APIs done

--- app/api/products/route.tsx
+++ app/api/products/route.tsx
@@ -1,13 +1,34 @@
 import { NextRequest, NextResponse } from 'next/server';
 import schema from './schema';
 
-export function GET(request: NextRequest) {
+export function GET(
+  request: NextRequest,
+  { params }: { params: { id: number } }
+) {
   // preventing caching by adding NextRequest
   return NextResponse.json([
     { id: 1, name: 'iphone', price: 2.5 },
     { id: 2, name: 'glove', price: 3.5 },
     { id: 3, name: 'hammer', price: 99.99 },
   ]);
+}
+
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: { id: number } }
+) {
+  const body = await request.json();
+
+  if (!body.name)
+    return NextResponse.json(
+      { error: 'Product name is required.' },
+      { status: 400 }
+    );
+
+  if (params.id > 10)
+    return NextResponse.json({ error: 'Product not found' }, { status: 404 });
+
+  return NextResponse.json({ id: 1, name: body.name });
 }
 
 export async function POST(request: NextRequest) {
@@ -24,4 +45,9 @@ export async function POST(request: NextRequest) {
 export function DELETE(
   request: NextRequest,
   { params }: { params: { id: number } }
-) {}
+) {
+  if (params.id > 10)
+    return NextResponse.json({ error: 'User not found' }, { status: 404 });
+
+  return NextResponse.json({});
+}
